cfa9f72f3a187125655a058cb9489661d0e59f35
snehal-joshi <59516532+snehal-joshi@users.noreply.github.com>
2022-11-05 21:58:13 -0500

    updated ToDoItem to send an id of the clicked on ToDo item

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,23 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    console.log("handleClick got called");
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-
-  return (
-    <li
-      onClick={handleClick}
-      style={{ textDecoration: isDone ? "line-through" : "none" }}
-    >
-      {props.item}
-    </li>
-  );
+  return <li onClick={() => props.onChecked(props.id)}>{props.item}</li>;
 }
 
 export default ToDoItem;
